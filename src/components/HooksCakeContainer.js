89ab1e9a48e78fe2cf19d3176ab71d5d470ac6c9
sahatilfat <sahatilfat@gmail.com>
2021-11-21 22:16:37 +0700

    useDispatch Hook

--- src/components/HooksCakeContainer.js
+++ src/components/HooksCakeContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { buyCake } from "../redux";
 
 function HooksCakeContainer() {
   const numOfCakes = useSelector((state) => state.numOfCakes);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h2>Num of Cakes - {numOfCakes}</h2>
-      <button>Buy cake</button>
+      <button onClick={() => dispatch(buyCake())}>Buy cake</button>
     </div>
   );
 }
