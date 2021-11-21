1e0bc8dc3425080cd43bce4b43f05987c6df7755
sahatilfat <sahatilfat@gmail.com>
2021-11-21 22:54:18 +0700

    Cakes and Ice Creams

---
+++ src/components/IceCreamContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+import { buyIceCream } from "../redux";
+
+function iceCreamContainer(props) {
+  return (
+    <div>
+      <h2>Number of icecream - {props.numOfIceCreams} </h2>
+      <button onClick={props.buyIceCream}>Buy icecream</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    numOfIceCreams: state.iceCream.numOfIceCreams,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyIceCream: () => dispatch(buyIceCream()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(iceCreamContainer);
